fe1cbfad08685784e26be030f987b5ebeaed4fd3
irena <irena.vent@gmail.com>
2021-09-10 00:36:23 +0200

    create criptovalue

---
+++ criptovalue/__init__.py
@@ -0,0 +1,2 @@
+APIKEY = "5EBCC73B-5DD2-43C3-BEB6-DF4BD45BC8DF"
+URL = "https://rest.coinapi.io/v1/exchangerate/{}/{}"
--- criptovalue/models.py
+++ criptovalue/models.py
@@ -1,4 +1,8 @@
 import requests
+from . import APIKEY, URL
+
+class APIError (Exception):
+    pass
 
 class CriptoValueModel():
     def __init__(self):
@@ -7,4 +11,13 @@ class CriptoValueModel():
         self.valor = 0.0
 
     def obtener(self):
-        
+        cabecera = {"X-CoinAPI-Key": apikey}
+        respuesta = requests.get(URL.format(self.de, self.a, headers = cabecera))
+
+        if respuesta.status_code == 200:
+            self.valor = respuesta.json()["rate"]
+        else:
+            print (respuesta.json())
+            raise APIError(f"Se ha producido el error {respuesta.status_code}")
+
+            
